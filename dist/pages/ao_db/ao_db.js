6ce343cf11b4a8fa207fafab074f4329767773cd
backlundpf <46462897+peterfbacklund@users.noreply.github.com>
2024-11-06 12:24:52 -0500

    don't commit dist

--- dist/pages/ao_db/ao_db.js
+++
@@ -1,9 +0,0 @@
-// src/pages/ao_db/ao_db.js
-console.log("hello from ao_db");
-function load(element) {
-  console.log("ao db executing: ", element);
-}
-return {
-  load
-};
-//# sourceMappingURL=ao_db.js.map
