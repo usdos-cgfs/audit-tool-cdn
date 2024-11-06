c8bb08b5f31702d31463d74ffb4201365095fee7
backlundpf <46462897+peterfbacklund@users.noreply.github.com>
2024-11-06 13:44:05 -0500

    add ao db as module

--- src/pages/ao_db/ao_db.js
+++ src/pages/ao_db/ao_db.js
@@ -1,9 +1,19 @@
-console.log("hello from ao_db");
+(function (root, factory) {
+  if (typeof define === "function" && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], factory);
+  } else {
+    // Browser globals
+    root.amdWeb = factory();
+  }
+})(typeof self !== "undefined" ? self : this, function () {
+  console.log("hello from ao_db");
 
-function load(element) {
-  console.log("ao db executing: ", element);
-}
+  function load(element, context) {
+    console.log("ao db executing: ", element);
+  }
 
-return {
-  load,
-};
+  return {
+    load,
+  };
+});
