1e0c398a93c63a2faaf46f854ba907880f114e53
Peter Backlund <backlundpf@state.gov>
2024-12-09 15:26:05 -0500

    add db taskdefs

--- src/tasks/db_tasks.js
+++ src/tasks/db_tasks.js
@@ -3,3 +3,12 @@ import { TaskDef } from "../services/index.js";
 export const loadingRemainder = new TaskDef("Loading Remainder Data", true);
 
 export const loadInfo = new TaskDef("Loading Initial Data");
+
+export const loadData = new TaskDef("Building Dashboard");
+
+// AO_DB
+
+export const submitPackageTaskDef = new TaskDef(
+  "Submitting Response Package",
+  true
+);
