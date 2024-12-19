bce011d5dbe807259d7d09f6c61ff483aa570ce9
peterfbacklund <backlundpf@state.gov>
2024-12-19 16:58:39 -0500

    Squashed commit of the following:
    commit b4345241e4c221b6b0ee26a8aeb800c80803d58e
    Author: peterfbacklund <backlundpf@state.gov>
    Date:   Thu Dec 19 09:49:32 2024 -0500
    
        use uniform script keys
    
    commit 95eb903c6de5efc109338490462a34d9d0d48d02
    Author: peterfbacklund <backlundpf@state.gov>
    Date:   Thu Dec 19 09:48:43 2024 -0500
    
        styled tabs
    
    commit 8868ebc74d8c265e6e53b632cd7fb788332e5a5d
    Author: peterfbacklund <backlundpf@state.gov>
    Date:   Wed Dec 18 15:49:26 2024 -0500
    
        start removing jquery requirement
    
        rebuild tabs interface
    
    commit 4607d7b0f9f85dac18717b6175fc3afd827d087c
    Author: peterfbacklund <backlundpf@state.gov>
    Date:   Tue Dec 17 15:22:31 2024 -0500
    
        add jquery dependency

--- src/pages/permissions/permissions.js
+++ src/pages/permissions/permissions.js
@@ -1,6 +1,16 @@
 ﻿import permissionsTemplate from "./permissions.html";
 import { NewUtilities } from "../../common/utilities.js";
 import { registerStyles } from "../../infrastructure/register_styles.js";
+// import $ from "../../../lib/jquery-bundle.js";
+import $ from "../../../lib/jquery-3.7.1.slim.js";
+// import { $, jQuery } from "jquery";
+// export for others scripts to use
+// window.$ = $;
+// window.jQuery = jQuery;
+// export default $;
+// import "jquery-ui-dist/jquery-ui.min.js";
+// import "jquery-ui-dist/jquery-ui.theme.min.css";
+import { tabs } from "../../../lib/tabs.js";
 
 var Audit = window.Audit || {
   Common: {},
@@ -119,7 +129,10 @@ Audit.Permissions.Load = function () {
 			
 			LoadDDOptions();
 		*/
-      $("#tabs").tabs().show();
+      // $("#tabs").tabs().show();
+      const tabsDiv = document.getElementById("tabs");
+      tabsDiv.style.display = "block";
+      tabs(tabsDiv);
 
       m_fnBindHandlersOnLoad();
 
@@ -128,7 +141,7 @@ Audit.Permissions.Load = function () {
 
       var isModalDlg = GetUrlKeyValue("IsDlg");
       if (isModalDlg == null || isModalDlg == "" || isModalDlg == false) {
-        $("#btnRefresh").show();
+        document.querySelector("#btnRefresh").style.display = "block";
       }
 
       var doneLoadingSPGroupPermissions = false;
@@ -140,7 +153,7 @@ Audit.Permissions.Load = function () {
       });
     }
     function OnFailure(sender, args) {
-      $("#divLoading").hide();
+      document.querySelector("#divLoading").style.display = "none";
 
       statusId = SP.UI.Status.addStatus(
         "Request failed: " + args.get_message() + "\n" + args.get_stackTrace()
@@ -870,9 +883,11 @@ Audit.Permissions.Load = function () {
   }
 
   function m_fnBindHandlerPermissionLinks() {
-    $(".permsLink").click(function () {
-      $(this).find(".permsInfo").toggleClass("collapsed");
-    });
+    document.querySelectorAll(".permsLink").forEach((link) =>
+      link.addEventListener("click", function () {
+        $(this).find(".permsInfo").toggleClass("collapsed");
+      })
+    );
   }
 
   function m_fnLoadSPGroupPermissions(OnCompleteLoading) {
@@ -1538,71 +1553,88 @@ Audit.Permissions.Load = function () {
 
     m_fnBindHandlerPermissionLinks();
 
-    $("#ddlRequestID").change(function () {
-      $("#ddlResponseRequestID").val($(this).val());
+    document
+      .querySelector("#ddlRequestID")
+      .addEventListener("change", function () {
+        $("#ddlResponseRequestID").val($(this).val());
 
-      setTimeout(function () {
-        FilterRequests();
-      }, 10);
-    });
+        setTimeout(function () {
+          FilterRequests();
+        }, 10);
+      });
 
-    $("#ddlResponseRequestID").change(function () {
-      $("#ddlRequestID").val($(this).val());
+    document
+      .querySelector("#ddlResponseRequestID")
+      .addEventListener("change", function () {
+        $("#ddlRequestID").val($(this).val());
 
-      setTimeout(function () {
-        FilterRequests();
-      }, 10);
-    });
+        setTimeout(function () {
+          FilterRequests();
+        }, 10);
+      });
 
-    $("#ddlResponseFolderResponseID").change(function () {
-      setTimeout(function () {
-        FilterResponses();
-      }, 10);
-    });
+    document
+      .querySelector("#ddlResponseFolderResponseID")
+      .addEventListener("change", function () {
+        setTimeout(function () {
+          FilterResponses();
+        }, 10);
+      });
 
-    $("#linkViewAO").click(function () {
-      m_fnViewAOs();
-    });
-    $("#linkAddAO").click(function () {
+    document
+      .querySelector("#linkViewAO")
+      .addEventListener("click", function () {
+        m_fnViewAOs();
+      });
+
+    document.querySelector("#linkAddAO").addEventListener("click", function () {
       m_fnAddAO();
     });
 
-    $("#linkUploadPermissions").click(function () {
-      m_fnUploadPermissions();
-    });
-
-    $("#linkGetVerification").click(function () {
-      m_fnGetVerification();
-    });
-
-    $("#linkEmailHistory").click(function () {
-      m_fnViewEmailHistoryFolder();
-    });
-
-    $("#linkViewExportFriendly").click(function () {
-      $(".removeOnExport").toggle();
-
-      $(".groupPerms").each(function () {
-        if ($(this).html().toLowerCase().indexOf("<ul>") >= 0) {
-          var friendlyNames = "";
-
-          $(this)
-            .find("LI")
-            .each(function () {
-              var curText = $(this).text();
-              var index = curText.indexOf("(");
-              if (index >= 0) curText = curText.substring(0, index);
-              curText = $.trim(curText);
-
-              friendlyNames += curText + ";";
-            });
-
-          $(this).html(friendlyNames);
-        }
+    document
+      .querySelector("#linkUploadPermissions")
+      .addEventListener("click", function () {
+        m_fnUploadPermissions();
       });
-    });
 
-    $("#cbAOAll").click(function () {
+    document
+      .querySelector("#linkGetVerification")
+      .addEventListener("click", function () {
+        m_fnGetVerification();
+      });
+
+    document
+      .querySelector("#linkEmailHistory")
+      .addEventListener("click", function () {
+        m_fnViewEmailHistoryFolder();
+      });
+
+    document
+      .querySelector("#linkViewExportFriendly")
+      .addEventListener("click", function () {
+        $(".removeOnExport").toggle();
+
+        $(".groupPerms").each(function () {
+          if ($(this).html().toLowerCase().indexOf("<ul>") >= 0) {
+            var friendlyNames = "";
+
+            $(this)
+              .find("LI")
+              .each(function () {
+                var curText = $(this).text();
+                var index = curText.indexOf("(");
+                if (index >= 0) curText = curText.substring(0, index);
+                curText = $.trim(curText);
+
+                friendlyNames += curText + ";";
+              });
+
+            $(this).html(friendlyNames);
+          }
+        });
+      });
+
+    document.querySelector("#cbAOAll").addEventListener("click", function () {
       if ($(this).is(":checked")) m_fnCheckCheckboxes();
       else m_fnUncheckCheckboxes();
     });
@@ -1705,16 +1737,18 @@ Audit.Permissions.Load = function () {
 
   function m_fnBindPrintButton(btnPrint, divTbl) {
     var pageTitle = "Audit Site Group Permissions (SharePoint Site)";
-    $(btnPrint).on("click", function () {
+    document.querySelector(btnPrint).addEventListener("click", function () {
       PrintPage(pageTitle, divTbl);
     });
   }
 
   function m_fnBindExportButton(btnExport, fileNamePrefix, tbl) {
-    $(btnExport).on("click", function (event) {
-      var curDate = new Date().format("yyyyMMdd");
-      ExportToCsv(fileNamePrefix + curDate, tbl);
-    });
+    document
+      .querySelector(btnExport)
+      .addEventListener("click", function (event) {
+        var curDate = new Date().format("yyyyMMdd");
+        ExportToCsv(fileNamePrefix + curDate, tbl);
+      });
   }
 
   //This function will grab the html from the container var to display in a new window.
