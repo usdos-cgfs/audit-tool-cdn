c5e644986e6699355eab214de9ca56f665820262
peterfbacklund <backlundpf@state.gov>
2025-02-08 10:40:17 -0500

    add support-email-ao config key and added to AO Dashboard

--- src/env.js
+++ src/env.js
@@ -1,2 +1,6 @@
 export const assetsPath = () =>
   `${window.context.pageContext.serverRelativeUrl}/Style Library/apps/audit/src`;
+
+export const CONFIGKEY = {
+  SUPPORTEMAILAO: "support-email-ao",
+};
